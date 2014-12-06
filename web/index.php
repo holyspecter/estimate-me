30d8a43e3920db3e15439d95fa0d097a822a908a
hospect <hospect@gmail.com>
2014-12-07 00:49:13 +0200

    getting questions and angular logic

--- web/index.php
+++ web/index.php
@@ -8,12 +8,33 @@ $app->get('/', function () use ($app) {
     return $app->sendFile('index.html');
 });
 
-$app->post('/', function () {
+$app->post('/answer', function () {
     // todo check answer an write to session
 });
 
-$app->get('/questions/{id}', function ($id) {
-    // todo return question by id
+$questions = [
+    "Запилить сайтик на WordPress'e, который будет уметь печь пирожки.",
+    "Интернет магазинчик с возможностью принждения к покупке посетителей.",
+];
+
+$answers = [
+    [
+        "За полчасика где-то",
+        "День-два, не больше",
+        "Около двух месяцев, в зависимости от начинки пирожков",
+    ],
+    [
+        "За часа три",
+        "День-два, не больше",
+        "Около года, также необходимо нанять титушек",
+    ],
+];
+
+$app->get('/questions/{id}', function ($id) use ($app, $questions, $answers) {
+    return $app->json([
+        "question" => $questions[$id],
+        "answers"  => $answers[$id],
+    ]);
 });
 
 $app->run();
--- web/js/controllers.js
+++ web/js/controllers.js
@@ -1,5 +1,34 @@
 var estimateMe = angular.module('estimateMe', []);
+var QUESTIONS_COUNT = 10;
 
-estimateMe.controller('MainController', function ($scope) {
+estimateMe.controller('MainController', function ($scope, $http) {
+    var questionsCounter = 0,
+        submittedAnswers = [],
+        ajaxLoader;
 
+
+    ajaxLoader = function () {
+        $http.get('questions/' + questionsCounter).success(function (data) {
+            console.log(data);
+
+            $scope.question = data.question;
+            $scope.answers = data.answers;
+        });
+    };
+
+    $scope.formData = {};
+
+    $scope.submit = function () {
+        submittedAnswers.push($scope.formData.submittedAnswer);
+
+        if (submittedAnswers.length < QUESTIONS_COUNT) {
+            questionsCounter++;
+
+            ajaxLoader();
+        } else {
+            $http.post('answer/', submittedAnswers);
+        }
+    };
+
+    ajaxLoader();
 });
