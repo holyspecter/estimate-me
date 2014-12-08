d40bdc3104cb2c9d9229fe28860d5e87db726ffc
hospect <hospect@gmail.com>
2014-12-09 00:50:50 +0200

    data extracted; results.html skeleton added

--- web/index.php
+++ web/index.php
@@ -8,29 +8,17 @@ $app->get('/', function () use ($app) {
     return $app->sendFile('index.html');
 });
 
-$app->post('/answer', function () {
-    // todo check answer an write to session
+$app->post('/answer', function () use ($app) {
+    $answers = require_once '../data/answers.php';
+
+//    todo maybe we really need twig here
+//    return $app->sendFile('results.html');
 });
 
-$questions = [
-    "Запилить сайтик на WordPress'e, который будет уметь печь пирожки.",
-    "Интернет магазинчик с возможностью принждения к покупке посетителей.",
-];
+$app->get('/questions/{id}', function ($id) use ($app) {
+    $questions = require_once '../data/questions.php';
+    $answers = require_once '../data/answers.php';
 
-$answers = [
-    [
-        "За полчасика где-то",
-        "День-два, не больше",
-        "Около двух месяцев, в зависимости от начинки пирожков",
-    ],
-    [
-        "За часа три",
-        "День-два, не больше",
-        "Около года, также необходимо нанять титушек",
-    ],
-];
-
-$app->get('/questions/{id}', function ($id) use ($app, $questions, $answers) {
     return $app->json([
         "question" => $questions[$id],
         "answers"  => $answers[$id],
